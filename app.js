25a3cc4a1a07482696d20872924e48b3af0ac267
samipkalyani <samipkalyani004@gmail.com>
2019-03-02 11:09:36 +0530

    second commit

--- app.js
+++ app.js
@@ -12,12 +12,39 @@ app.set('view engine', 'ejs');
 
 app.set('views', 'views');
 
-app.use('/',(reg,res)=>{
-    res.send("Hello");
+app.use('/auth',(req,res)=>{
+    res.render('auth');
+})
+
+app.post('/user_login',(req,res)=>{
+    res.render('user_login');
+})
+
+app.post('/projects_login',(req,res)=>{
+    res.render('projects_login');
+})
+
+app.post('/company_login',(req,res)=>{
+    res.render('company_login');
+})
+app.post('/xyz',(req,res)=>{
+    console.log(req.body);
+    res.send("COMPANY DETAILS FOUND");
+})
+
+app.get('/login_final',(req,res)=>{
+    
+    res.render('company_login_final');
+})
+app.post('/abc',(req,res)=>{
+    console.log(req.body);
+    res.send("COMPANY DETAILS FOUND");
 })
 
 
 
 
-
-app.listen(3000);
+const port =process.env.PORT|| 3000 ;
+app.listen(port,()=>{
+    console.log(`server-started on port ${port}`);
+});
